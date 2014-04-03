85e6d6f88876ec4c19dec648e39ff967f353271f
kaustubh-d <kaustubh@clogeny.com>
2014-04-03 16:30:24 +0530

    Azure handler manifest for *nix systems.

---
+++ ChefExtensionHandler/HandlerManifest.json.nix
@@ -0,0 +1,12 @@
+[{
+  "version": 1.0,
+  "handlerManifest": {
+    "installCommand": "install.sh",
+    "uninstallCommand": "uninstall.sh",
+    "updateCommand": "update.sh",
+    "enableCommand": "enable.sh",
+    "disableCommand": "disable.sh",
+    "rebootAfterInstall": false,
+    "reportHeartbeat": false
+  }
+}]
