a2272511dcd858a314d72aacceb83da02c392d56
kaustubh-d <kaustubh@clogeny.com>
2014-04-03 15:24:59 +0530

    chef-extn enable command that can be triggered by azure agent.

---
+++ lib/chef/azure/commands/enable.rb
@@ -0,0 +1,91 @@
+
+# This implements the azure extension 'enable' command.
+
+require 'chef'
+require 'chef/azure/helpers/shared'
+
+class EnableChef
+  include Chef::Mixin::ShellOut
+  include ChefAzure::Shared
+  include ChefAzure::Config
+  include ChefAzure::Reporting
+
+  def initialize(extension_root, *enable_args)
+    @chef_extension_root = extension_root
+    @enable_args = enable_args
+    @exit_code = 0
+  end
+
+  def run
+    load_env
+
+    report_heart_beat_to_azure(@azure_heart_beat_file, AzureHeartBeat::NOTREADY, 0, "Enabling chef-service...")
+
+    enable_chef
+
+    if @exit_code == 0
+      report_heart_beat_to_azure(@azure_heart_beat_file, AzureHeartBeat::READY, 0, "chef-service is enabled")
+    else
+      report_heart_beat_to_azure(@azure_heart_beat_file, AzureHeartBeat::NOTREADY, 0, "chef-service enable failed")
+    end
+
+    return @exit_code
+
+  end
+
+  private
+  def load_env
+    @azure_heart_beat_file, @azure_status_folder, @azure_plugin_log_location, @azure_config_folder, @azure_status_file = read_config(@chef_extension_root)
+  end
+
+  def enable_chef
+    # Enabling Chef involves following steps:
+    # - Configure chef only on first run
+    # - Install the Chef service
+    # - Start the Chef service   
+    begin
+      configure_chef_only_once
+
+      install_chef_service if @exit_code == 0
+
+      enable_chef_service if @exit_code == 0
+
+    rescue => e
+      Chef::Log.error e
+      report_status_to_azure "#{e} - Check log file for details", "error"
+      @exit_code = 1
+    ensure
+      # Once process exits, we log the current process' pid
+      Chef::Log.info "Process completed (pid: #{Process.pid})"
+    end
+    @exit_code
+  end
+
+  def install_chef_service
+    @exit_code, error_message = ChefService.install(@azure_plugin_log_location)
+    if @exit_code == 0
+      report_status_to_azure "chef-service installed", "success"
+    else
+      report_status_to_azure "chef-service install failed - #{error_message}", "success"
+    end
+    @exit_code
+  end
+
+  def enable_chef_service
+    @exit_code, error_message = ChefService.enable(@azure_plugin_log_location)
+    if @exit_code == 0
+      report_status_to_azure "chef-service enabled", "success"
+    else
+      report_status_to_azure "chef-service enable failed - #{error_message}", "success"
+    end
+    @exit_code
+  end
+
+  # Configuring chef involves
+  #   => create bootstrap folder with client.rb, validation.pem, first_boot.json
+  #   => Perform node registration executing first chef run
+  #   => run the user supplied runlist from first_boot.json in async manner
+  def configure_chef_only_once
+
+  end
+end
