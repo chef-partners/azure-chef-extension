92242ccd722538a7ae6349650b4ec661c27bceb1
prabhu-das <prabhu.das@clogeny.com>
2014-04-07 18:40:36 +0530

    Refactored code

--- ChefExtensionHandler/bin/chef-uninstall.sh
+++ ChefExtensionHandler/bin/chef-uninstall.sh
@@ -7,12 +7,12 @@
 
 # Uninstall the custom gem
 export PATH=$PATH:/opt/chef/embedded/bin
-azure_chef_extn_gem=`gem list | grep azure-chef-extension | awk '{print $1}'`
+azure_chef_extn_gem=`gem list azure-chef-extension | grep azure-chef-extension | awk '{print $1}'`
 if test "$azure_chef_extn_gem" = "azure-chef-extension" ; then
   echo "Started removing gem azure-chef-extension"
   uninstall_gem=`gem uninstall azure-chef-extension`
   if [ $? -eq 0 ]; then
-    echo "Gem $azure_chef_extn_gem_status uninstalled successfully :)"
+    echo "Gem $azure_chef_extn_gem_status uninstalled successfully."
   else
     echo "Unable to uninstall gem azure-chef-extension."
   fi
@@ -27,7 +27,7 @@ if test "$install_status" = "ii" ; then
   echo "Started removing Chef."
   uninstall=`sudo dpkg -P $pkg_name`
   if [ $? -eq 0 ]; then
-    echo "Package $pkg_name uninstalled successfully :)"
+    echo "Package $pkg_name uninstalled successfully."
   else
     echo "Unable to uninstall package Chef."
   fi
