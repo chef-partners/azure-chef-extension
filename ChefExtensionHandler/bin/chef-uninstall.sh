1bee5147f12b5180bbc4472fc301006234d6e2a7
prabhu-das <prabhu.das@clogeny.com>
2014-04-08 14:24:38 +0530

    Implemented review comments.

--- ChefExtensionHandler/bin/chef-uninstall.sh
+++ ChefExtensionHandler/bin/chef-uninstall.sh
@@ -1,31 +1,26 @@
-# Author:: Prabhu Das (<prabhu.das@clogeny.com>)
-# Copyright:: Copyright (c) 2014 Opscode, Inc.
-
-# uninstall chef
-# Actions:
-#    - uninstall chef
+#!/bin/sh
 
 # Uninstall the custom gem
-export PATH=$PATH:/opt/chef/embedded/bin
+export PATH=$PATH:/opt/chef/embedded/bin:/opt/chef/bin
 azure_chef_extn_gem=`gem list azure-chef-extension | grep azure-chef-extension | awk '{print $1}'`
+
 if test "$azure_chef_extn_gem" = "azure-chef-extension" ; then
-  echo "Started removing gem azure-chef-extension"
-  uninstall_gem=`gem uninstall azure-chef-extension`
-  if [ $? -eq 0 ]; then
-    echo "Gem $azure_chef_extn_gem_status uninstalled successfully."
-  else
+  echo "Removing azure-chef-extension gem."
+  gem uninstall azure-chef-extension
+  if [ $? -ne 0 ]; then
     echo "Unable to uninstall gem azure-chef-extension."
   fi
 else
-  echo "Gem azure-chef-extension not found !!!" 
+  echo "Gem azure-chef-extension is not installed !!!" 
 fi
 
 # Uninstall chef_pkg
-install_status=`dpkg -l | grep chef | awk '{print $1}'`
 pkg_name=`dpkg -l | grep chef | awk '{print $2}'`
-if test "$install_status" = "ii" ; then
-  echo "Started removing Chef."
-  uninstall=`sudo dpkg -P $pkg_name`
+dpkg_installed="Status: install ok installed"
+install_status=`dpkg -s "$pkg_name" | grep "$dpkg_installed"`
+
+if test "$install_status" = "$dpkg_installed" ; then
+  dpkg -P $pkg_name
   if [ $? -eq 0 ]; then
     echo "Package $pkg_name uninstalled successfully."
   else
@@ -34,4 +29,3 @@ if test "$install_status" = "ii" ; then
 else
   echo "No Package found to uninstall!!!"
 fi
-
