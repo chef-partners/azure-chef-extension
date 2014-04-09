a8b8c192ed5cfafb6970405554748ce471a65df7
prabhu-das <prabhu.das@clogeny.com>
2014-04-09 16:17:47 +0530

    Changes for update.sh

--- ChefExtensionHandler/bin/chef-uninstall.sh
+++ ChefExtensionHandler/bin/chef-uninstall.sh
@@ -1,31 +1,38 @@
 #!/bin/sh
 
-# Uninstall the custom gem
-export PATH=$PATH:/opt/chef/embedded/bin:/opt/chef/bin
-azure_chef_extn_gem=`gem list azure-chef-extension | grep azure-chef-extension | awk '{print $1}'`
+update_process_descriptor=/etc/chef/.update_process_descriptor
 
-if test "$azure_chef_extn_gem" = "azure-chef-extension" ; then
-  echo "Removing azure-chef-extension gem."
-  gem uninstall azure-chef-extension
-  if [ $? -ne 0 ]; then
-    echo "Unable to uninstall gem azure-chef-extension."
-  fi
+if [ -f $update_process_descriptor ]; then
+  echo "Not tried to uninstall, as the update process is running"
+  rm $update_process_descriptor
 else
-  echo "Gem azure-chef-extension is not installed !!!" 
-fi
+  # Uninstall the custom gem
+  export PATH=$PATH:/opt/chef/embedded/bin:/opt/chef/bin
+  azure_chef_extn_gem=`gem list azure-chef-extension | grep azure-chef-extension | awk '{print $1}'`
 
-# Uninstall chef_pkg
-pkg_name=`dpkg -l | grep chef | awk '{print $2}'`
-dpkg_installed="Status: install ok installed"
-install_status=`dpkg -s "$pkg_name" | grep "$dpkg_installed"`
-
-if test "$install_status" = "$dpkg_installed" ; then
-  dpkg -P $pkg_name
-  if [ $? -eq 0 ]; then
-    echo "Package $pkg_name uninstalled successfully."
+  if test "$azure_chef_extn_gem" = "azure-chef-extension" ; then
+    echo "Removing azure-chef-extension gem."
+    gem uninstall azure-chef-extension
+    if [ $? -ne 0 ]; then
+      echo "Unable to uninstall gem azure-chef-extension."
+    fi
   else
-    echo "Unable to uninstall package Chef."
+    echo "Gem azure-chef-extension is not installed !!!" 
   fi
-else
-  echo "No Package found to uninstall!!!"
+
+  # Uninstall chef_pkg
+  pkg_name=`dpkg -l | grep chef | awk '{print $2}'`
+  dpkg_installed="Status: install ok installed"
+  install_status=`dpkg -s "$pkg_name" | grep "$dpkg_installed"`
+
+  if test "$install_status" = "$dpkg_installed" ; then
+    dpkg -P $pkg_name
+    if [ $? -eq 0 ]; then
+      echo "Package $pkg_name uninstalled successfully."
+    else
+      echo "Unable to uninstall package Chef."
+    fi
+  else
+    echo "No Package found to uninstall!!!"
+  fi
 fi
