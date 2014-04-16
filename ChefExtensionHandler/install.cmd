53e6ef0a33746d32208a14ab6b5c430f0089c145
siddheshwar-more <siddheshwar.more@clogeny.com>
2014-04-16 18:24:23 +0530

    Implemented changes to convert plain chef-install PS into modular format

--- ChefExtensionHandler/install.cmd
+++ ChefExtensionHandler/install.cmd
@@ -3,4 +3,4 @@ set CHEF_EXT_DIR=%~dp0
 
 echo %CHEF_EXT_DIR%
 
-powershell -nologo -noprofile -executionpolicy unrestricted -File %CHEF_EXT_DIR%bin\chef-install.ps1
+powershell -nologo -noprofile -executionpolicy unrestricted Import-Module %CHEF_EXT_DIR%bin\chef-install.psm1;Install-ChefClient
