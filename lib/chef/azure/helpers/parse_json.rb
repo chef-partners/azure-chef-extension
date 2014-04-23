d8bc6bf1c6e8637cd4052e787bbb7cc019f18054
siddheshwar-more <siddheshwar.more@clogeny.com>
2014-04-23 17:56:10 +0530

    Added support to get value from powershell for ruby helper value_from_json_file

--- lib/chef/azure/helpers/parse_json.rb
+++ lib/chef/azure/helpers/parse_json.rb
@@ -118,6 +118,6 @@ def value_from_json_file(file_name, *keys)
   if ! json_value.is_a?(String)
     raise ArgumentError, "Specified keys #{keys.to_s} retrieved an object of type #{json_value.class} instead of a String. Retrieved value was a(n) #{json_value.class.to_s}"
   end
-
+  print json_value
   json_value
 end
