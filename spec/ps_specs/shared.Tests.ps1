36928b186e6ae4e3ea9ce69895ea4688a2234160
siddheshwar-more <siddheshwar.more@clogeny.com>
2014-04-24 13:37:37 +0530

    Updated pester tests for shared helper

--- spec/ps_specs/shared.Tests.ps1
+++ spec/ps_specs/shared.Tests.ps1
@@ -23,17 +23,10 @@ describe "#Read-JsonFile" {
     mock Get-HandlerSettings {return $handlerSettings}
     mock Get-HandlerEnvironment {return $handlerEnvironment}
 
-    $json_handlerSettingsFileName, $json_handlerSettings, $json_protectedSettings,  $json_protectedSettingsCertThumbprint, $json_client_rb , $json_runlist, $json_chefLogFolder, $json_statusFolder, $json_heartbeatFile = Read-JsonFile
+    $json_handlerSettingsFileName, $json_statusFolder = Read-JsonFile
 
     $json_handlerSettingsFileName | Should Be $handlerSettingsFileName
-    $json_handlerSettings | Should Be $handlerSettings
-    $json_protectedSettings | Should Be $handlerSettings.protectedSettings
-    $json_protectedSettingsCertThumbprint | Should Be $handlerSettings.protectedSettingsCertThumbprint
-    $json_client_rb | Should Be $handlerSettings.publicSettings.client_rb
-    $json_runlist | Should Be $handlerSettings.publicSettings.runList
-    $json_chefLogFolder | Should Be $handlerEnvironment.logFolder
     $json_statusFolder | Should Be $handlerEnvironment.statusFolder
-    $json_heartbeatFile | Should Be $handlerEnvironment.heartbeatFile
   }
 }
 
@@ -45,29 +38,14 @@ describe "#Read-JsonFileUsingRuby" {
     $handlerEnvironmentFilePath = "testhandlerEnvironmentFilePath"
     mock Get-HandlerEnvironmentFilePath { return $handlerEnvironmentFilePath }
 
-    $handlerSettings = @{"protectedSettings" = "testprotectedSettings"; "protectedSettingsCertThumbprint" = "testprotectedSettingsCertThumbprint"; "publicSettings" = @{"client_rb"= "testclientrb"; "runList" = "testrunlist"}}
     $handlerEnvironment = @{"logFolder" = "testlogFolder"; "statusFolder" = "teststatusFolder"; "heartbeatFile" = "testheartbeatFile" }
 
-    mock Get-JsonValueUsingRuby {return $handlerSettings } -ParameterFilter { $json_handlerSettingsFileName -eq $handlerSettingsFilePath -and $args[0] -eq "runtimeSettings" -and $args[1] -eq "0" -and $args[2] -eq "handlerSettings" }
-    mock Get-JsonValueUsingRuby {return $handlerSettings.protectedSettings } -ParameterFilter { $json_handlerSettingsFileName -eq $handlerSettingsFilePath -and $args[0] -eq "runtimeSettings" -and $args[1] -eq "0" -and $args[2] -eq "handlerSettings" -and $args[3] -eq "protectedSettings" }
-    mock Get-JsonValueUsingRuby {return $handlerSettings.protectedSettingsCertThumbprint } -ParameterFilter { $json_handlerSettingsFileName -eq $handlerSettingsFilePath -and $args[0] -eq "runtimeSettings" -and $args[1] -eq "0" -and $args[2] -eq "handlerSettings" -and $args[3] -eq "protectedSettingsCertThumbprint" }
-    mock Get-JsonValueUsingRuby {return $handlerSettings.publicSettings.client_rb } -ParameterFilter { $json_handlerSettingsFileName -eq $handlerSettingsFilePath -and $args[0] -eq "runtimeSettings" -and $args[1] -eq "0" -and $args[2] -eq "handlerSettings" -and $args[3] -eq "publicSettings" -and $args[4] -eq "client_rb"}
-    mock Get-JsonValueUsingRuby {return $handlerSettings.publicSettings.runList } -ParameterFilter { $json_handlerSettingsFileName -eq $handlerSettingsFilePath -and $args[0] -eq "runtimeSettings" -and $args[1] -eq "0" -and $args[2] -eq "handlerSettings" -and $args[3] -eq "publicSettings" -and $args[4] -eq "runList" }
-    mock Get-JsonValueUsingRuby {return $handlerEnvironment.logFolder } -ParameterFilter { $json_handlerEnvironmentFileName -eq $handlerEnvironmentFilePath -and $args[0] -eq "handlerEnvironment" -and $args[1] -eq "logFolder" }
     mock Get-JsonValueUsingRuby {return $handlerEnvironment.statusFolder } -ParameterFilter { $json_handlerEnvironmentFileName -eq $handlerEnvironmentFilePath -and $args[0] -eq "handlerEnvironment" -and $args[1] -eq "statusFolder" }
-    mock Get-JsonValueUsingRuby {return $handlerEnvironment.heartbeatFile } -ParameterFilter { $json_handlerEnvironmentFileName -eq $handlerEnvironmentFilePath -and $args[0] -eq "handlerEnvironment" -and $args[1] -eq "heartbeatFile" }
 
-    $json_handlerSettingsFileName, $json_handlerSettings, $json_protectedSettings,  $json_protectedSettingsCertThumbprint, $json_client_rb , $json_runlist, $json_chefLogFolder, $json_statusFolder, $json_heartbeatFile = Read-JsonFileUsingRuby
+    $json_handlerSettingsFileName, $json_statusFolder = Read-JsonFileUsingRuby
 
     $json_handlerSettingsFileName | Should Be $handlerSettingsFilePath
-    $json_handlerSettings | Should Be $handlerSettings
-    $json_protectedSettings | Should Be $handlerSettings.protectedSettings
-    $json_protectedSettingsCertThumbprint | Should Be $handlerSettings.protectedSettingsCertThumbprint
-    $json_client_rb | Should Be $handlerSettings.publicSettings.client_rb
-    $json_runlist | Should Be $handlerSettings.publicSettings.runList
-    $json_chefLogFolder | Should Be $handlerEnvironment.logFolder
     $json_statusFolder | Should Be $handlerEnvironment.statusFolder
-    $json_heartbeatFile | Should Be $handlerEnvironment.heartbeatFile
   }
 }
 
