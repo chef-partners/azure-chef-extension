587b6e03e1977347a9458c6f90ae983eed5fa88a
kaustubh-d <kaustubh@clogeny.com>
2014-04-30 20:42:44 +0530

    implement publishing of extn zip using PS.

--- scripts/publishpkg.psm1
+++ scripts/publishpkg.psm1
@@ -1,8 +1,16 @@
-function Publish-ChefPkg($publishSettingsFile, $publishUri, $definitionXmlFile) {
+function Publish-ChefPkg($publishSettingsFile, $subscriptionName, $publishUri, $definitionXmlFile) {
   trap [Exception] {echo $_.Exception.Message;exit 1}
 
-  write-host "Publish-ChefPkg called with: $publishSettingsFile, $publishUri, $definitionXmlFile"
+  write-host "Publish-ChefPkg called with: $publishSettingsFile, $subscriptionName, $publishUri, $definitionXmlFile"
 
+  Import-AzurePublishSettingsFile -PublishSettingsFile $publishSettingsFile
+
+  $subscription = Get-AzureSubscription –SubscriptionName $subscriptionName
+
+  $bodyxml = Get-Content $definitionXmlFile
+
+  # Trigger the publish
+  Invoke-RestMethod -Method POST -Uri $publishUri -Certificate $subscription.Certificate -Headers @{'x-ms-version'='2014-04-01'} -Body $bodyxml -ContentType application/xml;
 }
 
 Export-ModuleMember -Function Publish-ChefPkg
