839a119010deca0b27dc2084b4ad9c3c5e80ad06
kaustubh-d <kaustubh@clogeny.com>
2014-05-02 12:03:54 +0530

    use put method to update extension.

--- scripts/publishpkg.psm1
+++ scripts/publishpkg.psm1
@@ -1,16 +1,16 @@
 function Publish-ChefPkg($publishSettingsFile, $subscriptionName, $publishUri, $definitionXmlFile) {
   trap [Exception] {echo $_.Exception.Message;exit 1}
 
-  write-host "Publish-ChefPkg called with: $publishSettingsFile, $subscriptionName, $publishUri, $definitionXmlFile"
+  write-host "Publish-ChefPkg called with: $publishSettingsFile, `'$subscriptionName`', $publishUri, $definitionXmlFile"
 
   Import-AzurePublishSettingsFile -PublishSettingsFile $publishSettingsFile
 
-  $subscription = Get-AzureSubscription –SubscriptionName $subscriptionName
+  $subscription = Get-AzureSubscription -SubscriptionName $subscriptionName
 
   $bodyxml = Get-Content $definitionXmlFile
 
   # Trigger the publish
-  Invoke-RestMethod -Method POST -Uri $publishUri -Certificate $subscription.Certificate -Headers @{'x-ms-version'='2014-04-01'} -Body $bodyxml -ContentType application/xml;
+  Invoke-RestMethod -Method PUT -Uri $publishUri -Certificate $subscription.Certificate -Headers @{'x-ms-version'='2014-04-01'} -Body $bodyxml -ContentType application/xml;
 }
 
 Export-ModuleMember -Function Publish-ChefPkg
