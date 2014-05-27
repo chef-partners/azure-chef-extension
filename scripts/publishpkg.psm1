1224e34568cca195882f3bea5a1be689af0c646c
kaustubh-d <kaustubh@clogeny.com>
2014-05-27 15:54:09 +0530

    poll for publish status.

--- scripts/publishpkg.psm1
+++ scripts/publishpkg.psm1
@@ -10,7 +10,45 @@ function Publish-ChefPkg($publishSettingsFile, $subscriptionName, $publishUri, $
   $bodyxml = Get-Content $definitionXmlFile
 
   # Trigger the publish
-  Invoke-RestMethod -Method $postOrPut -Uri $publishUri -Certificate $subscription.Certificate -Headers @{'x-ms-version'='2014-04-01'} -Body $bodyxml -ContentType application/xml;
+  $req = Invoke-WebRequest -Method $postOrPut -Uri $publishUri -Certificate $subscription.Certificate -Headers @{'x-ms-version'='2014-04-01'} -Body $bodyxml -ContentType application/xml;
+
+  # retry status retrieval for few times.
+  $uri = $subscription.ServiceEndpoint.AbsoluteUri
+  $subscriptionId = $subscription.subscriptionId
+  $reqId = $req.Headers['x-ms-request-id']
+  $reqUri = "${uri}${subscriptionId}/operations/${reqId}"
+  $retryCount = 5
+  while ($true) {
+    write-host "Checking publish status with operation id ($reqId)..."
+    $reqStatus = Invoke-WebRequest -Method GET -Uri $reqUri -Certificate $subscription.Certificate -Headers @{'x-ms-version'='2014-04-01'}
+    $xmlResponse = [xml]$reqStatus.Content
+    switch ($xmlResponse.Operation.Status) {
+      "Succeeded" {
+        write-host "Successfully published."
+        $retryCount = 0 # exit while
+        break
+        }
+      "InProgress" {
+        # Just keep retrying.
+        write-host "Publishing Inprogress.`nWaiting for 10 seconds..."
+        Start-Sleep -s 10
+        break
+        }
+      "Failed" {
+        write-host "Failed publishing."
+        write-host $reqStatus.Content
+        $retryCount = 0 # exit while
+        break
+        }
+      default {
+        write-host "Unknown error during publishing."
+        write-host $reqStatus.Content
+        $retryCount = 0 # exit while
+        break
+      }
+    }# switch
+    if ($retryCount -eq 0) { break } else { $retryCount-- }
+  }
 }
 
 Export-ModuleMember -Function Publish-ChefPkg
