767164def67c569061bde332ed59230d1139dbe7
kaustubh-d <kaustubh@clogeny.com>
2014-05-29 14:50:43 +0530

    add support to delete published pkgs

---
+++ scripts/deletepkg.psm1
@@ -0,0 +1,26 @@
+
+function Get-CurrentScriptDirectory
+{
+  $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+  Split-Path $Invocation.MyCommand.Path
+}
+
+$scriptDir = Get-CurrentScriptDirectory
+
+Import-Module "$scriptDir\shared.psm1"
+
+function Delete-ChefPkg($publishSettingsFile, $subscriptionName, $deleteUri) {
+  trap [Exception] {echo $_.Exception.Message;exit 1}
+
+  write-host "Delete-ChefPkg called with: $publishSettingsFile, `'$subscriptionName`', $deleteUri"
+
+  Import-AzurePublishSettingsFile -PublishSettingsFile $publishSettingsFile
+
+  $subscription = Get-AzureSubscription -SubscriptionName $subscriptionName
+
+  $req = Invoke-WebRequest -Method DELETE -Uri $deleteUri -Certificate $subscription.Certificate -Headers @{'x-ms-version'='2014-04-01'} -Body $bodyxml -ContentType application/xml;
+
+  Poll-AzureOperationStatus $req.Headers['x-ms-request-id'] $subscription
+}
+
+Export-ModuleMember -Function Delete-ChefPkg
