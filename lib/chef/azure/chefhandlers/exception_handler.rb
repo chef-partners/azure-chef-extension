4dbec0e8c4ff39700f70a9c99ce634c321cdae28
nimisha <nimisha.sharad@clogeny.com>
2015-02-12 18:21:07 +0530

    Save runlist if first chef-client run fails

--- lib/chef/azure/chefhandlers/exception_handler.rb
+++ lib/chef/azure/chefhandlers/exception_handler.rb
@@ -1,5 +1,6 @@
 require 'chef/log'
 require 'chef/azure/helpers/shared'
+require 'json'
 
 module AzureExtension
   class ExceptionHandler < Chef::Handler
@@ -13,11 +14,19 @@ module AzureExtension
 
     def report
       if run_status.failed?
+        load_run_list if not File.exists?("#{bootstrap_directory}/node-registered")
         load_azure_env
         message = "Check log file for details...\nBacktrace:\n"
         message << Array(backtrace).join("\n")
-        report_heart_beat_to_azure(AzureHeartBeat::READY, 0, "chef-service is running properly. Chef client run failed with error- #{message}") 
+        report_heart_beat_to_azure(AzureHeartBeat::READY, 0, "chef-service is running properly. Chef client run failed with error- #{message}")
       end
+    end
+
+    def load_run_list
+      first_boot = File.read("#{bootstrap_directory}/first-boot.json")
+      first_boot = JSON.parse(first_boot)
+      run_list = first_boot["run_list"]
+      node.run_list.reset!(run_list)
     end
   end
 end
