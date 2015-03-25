8ffdac813994410e05a65a750f4c904ac24bc1cb
nimisha <nimisha.sharad@clogeny.com>
2015-03-25 18:19:42 +0530

    Removed depricated rspec syntaxes

--- spec/unit/azure_enable_spec.rb
+++ spec/unit/azure_enable_spec.rb
@@ -69,82 +69,82 @@ describe EnableChef do
 
   context "load_env" do
     it "loads azure specific environment configurations from config file." do
-      instance.should_receive(:read_config)
+      expect(instance).to receive(:read_config)
       instance.send(:load_env)
     end
   end
 
   context "enable_chef" do
     it "configures, installs and enables chef." do
-      instance.should_receive(:configure_chef_only_once)
-      instance.should_receive(:install_chef_service)
-      instance.should_receive(:enable_chef_service)
+      expect(instance).to receive(:configure_chef_only_once)
+      expect(instance).to receive(:install_chef_service)
+      expect(instance).to receive(:enable_chef_service)
       instance.send(:enable_chef)
     end
   end
 
   context "install_chef_service" do
     it "installs the chef service and returns the status to azure." do
-      instance.should_receive(:report_status_to_azure).with("chef-service installed", "success")
-      ChefService.stub_chain(:new, :install).and_return(0)
+      expect(instance).to receive(:report_status_to_azure).with("chef-service installed", "success")
+      allow(ChefService).to receive_message_chain(:new, :install).and_return(0)
       instance.send(:install_chef_service)
     end
 
     it "installs the chef service and returns the status to azure." do
-      instance.should_receive(:report_status_to_azure).with("chef-service install failed - ", "error")
-      ChefService.stub_chain(:new, :install).and_return(1)
+      expect(instance).to receive(:report_status_to_azure).with("chef-service install failed - ", "error")
+      allow(ChefService).to receive_message_chain(:new, :install).and_return(1)
       instance.send(:install_chef_service)
     end
   end
 
   context "enable_chef_service" do
     it "enables the chef service and returns the status to azure." do
-      instance.should_receive(:report_status_to_azure).with("chef-service enabled", "success")
-      ChefService.stub_chain(:new, :enable).and_return(0)
+      expect(instance).to receive(:report_status_to_azure).with("chef-service enabled", "success")
+      allow(ChefService).to receive_message_chain(:new, :enable).and_return(0)
       instance.send(:enable_chef_service)
     end
 
     it "enables the chef service and returns the status to azure." do
-      instance.should_receive(:report_status_to_azure).with("chef-service enable failed - ", "error")
-      ChefService.stub_chain(:new, :enable).and_return(1)
+      expect(instance).to receive(:report_status_to_azure).with("chef-service enable failed - ", "error")
+      allow(ChefService).to receive_message_chain(:new, :enable).and_return(1)
       instance.send(:enable_chef_service)
     end
   end
 
   context "configure_chef_only_once" do
     it "runs the chef-client for the first time" do
-      instance.stub(:puts)
-      instance.stub(:shell_out).and_return(OpenStruct.new(:exitstatus => 0, :stdout => ""))
-      File.stub_chain(:open, :write).and_return(true)
-      instance.stub(:load_settings)
-      Process.stub(:spawn)
-      Process.stub(:detach)
-      instance.stub(:bootstrap_directory).and_return(Dir.home)
+      allow(instance).to receive(:puts)
+      allow(instance).to receive(:shell_out).and_return(OpenStruct.new(:exitstatus => 0, :stdout => ""))
+      allow(File).to receive_message_chain(:open, :write).and_return(true)
+      allow(instance).to receive(:load_settings)
+      allow(Process).to receive(:spawn)
+      allow(Process).to receive(:detach)
+      allow(instance).to receive(:bootstrap_directory).and_return(Dir.home)
       instance.send(:configure_chef_only_once)
     end
   end
 
   context "load_settings" do
     it "loads the settings from the handler settings file." do
-      instance.should_receive(:handler_settings_file).exactly(3).times
-      instance.should_receive(:value_from_json_file).exactly(3).times
-      instance.should_receive(:get_validation_key)
+      expect(instance).to receive(:handler_settings_file).exactly(3).times
+      expect(instance).to receive(:value_from_json_file).exactly(3).times
+      expect(instance).to receive(:get_validation_key)
       instance.send(:load_settings)
     end
   end
 
   context "handler_settings_file" do
     it "returns the handler settings file when the settings file is present." do
-      Dir.stub_chain(:glob, :sort).and_return ["test"]
-      File.should_receive(:expand_path)
+      allow(Dir).to receive_message_chain(:glob, :sort).and_return ["test"]
+      expect(File).to receive(:expand_path)
       instance.send(:handler_settings_file)
     end
 
     it "returns error message when the settings file is not present." do
-      Dir.stub_chain(:glob, :sort).and_return []
-      File.should_receive(:expand_path)
-      Chef::Log.stub(:error)
-      instance.should_receive(:report_status_to_azure)
+      allow(Dir).to receive_message_chain(:glob, :sort).and_return []
+      expect(File).to receive(:expand_path)
+      allow(Chef::Log).to receive(:error)
+      expect(instance).to receive(:report_status_to_azure)
       expect {instance.send(:handler_settings_file)}.to raise_error
     end
   end
@@ -158,25 +158,25 @@ describe EnableChef do
   context "get_validation_key on linux" , :unless => (RUBY_PLATFORM =~ /mswin|mingw|windows/) do
     it "extracts and returns the validation_key from encrypted text." do
       @object = Object.new
-      File.stub(:read)
-      OpenSSL::X509::Certificate.stub(:new)
-      OpenSSL::PKey::RSA.stub(:new).and_return(@object)
-      Base64.stub(:decode64)
-      OpenSSL::PKCS7.stub(:new).and_return(@object)
-      @object.should_receive(:decrypt)
-      @object.should_receive(:to_pem)
-      instance.should_receive(:value_from_json_file).once.and_return("")
+      allow(File).to receive(:read)
+      allow(OpenSSL::X509::Certificate).to receive(:new)
+      allow(OpenSSL::PKey::RSA).to receive(:new).and_return(@object)
+      allow(Base64).to receive(:decode64)
+      allow(OpenSSL::PKCS7).to receive(:new).and_return(@object)
+      expect(@object).to receive(:decrypt)
+      expect(@object).to receive(:to_pem)
+      expect(instance).to receive(:value_from_json_file).once.and_return("")
       instance.send(:get_validation_key, "encrypted_text")
     end
   end
 
   context "get_validation_key on windows" , :if => (RUBY_PLATFORM =~ /mswin|mingw|windows/) do
     it "extracts and returns the validation_key from encrypted text." do
-      File.stub(:expand_path).and_return(".")
-      File.stub(:dirname)
-      instance.stub(:shell_out).and_return(OpenStruct.new(:exitstatus => 0, :stdout => ""))
-      instance.should_receive(:handler_settings_file)
-      instance.should_receive(:value_from_json_file).twice.and_return("")
+      allow(File).to receive(:expand_path).and_return(".")
+      allow(File).to receive(:dirname)
+      allow(instance).to receive(:shell_out).and_return(OpenStruct.new(:exitstatus => 0, :stdout => ""))
+      expect(instance).to receive(:handler_settings_file)
+      expect(instance).to receive(:value_from_json_file).twice.and_return("")
       instance.send(:get_validation_key, "encrypted_text")
     end
   end
--- spec/unit/parse_json_spec.rb
+++ spec/unit/parse_json_spec.rb
@@ -3,7 +3,7 @@ require 'chef/azure/helpers/parse_json'
 
 describe JSONFileReader do
   file_content = '[{ "name": "Test", "version": "1.0" }]'
-  let (:instance) {JSONFileReader.new(file_content, 'name', 'version'){|obj| obj.should_receive(:deserialize_json).and_return(true)}}
+  let (:instance) {JSONFileReader.new(file_content, 'name', 'version'){|obj| expect(obj).to receive(:deserialize_json).and_return(true)}}
   it { expect {instance}.to_not raise_error }
   it { expect {instance.read_value}.to_not raise_error }
   it { expect {instance.get_deserialized_objects}.to_not raise_error }
@@ -11,26 +11,26 @@ describe JSONFileReader do
 
   context "is_alphanumeric" do
    it "returns true if the sequence is a alphabatic/numeric/alphanumeric string, false otherwise." do
-   instance.send(:is_alphanumeric, "123").should be true
-   instance.send(:is_alphanumeric, "file").should be true
-   instance.send(:is_alphanumeric, "file123").should be true
-   instance.send(:is_alphanumeric, " ").should be false
+   expect(instance.send(:is_alphanumeric, "123")).to be true
+   expect(instance.send(:is_alphanumeric, "file")).to be true
+   expect(instance.send(:is_alphanumeric, "file123")).to be true
+   expect(instance.send(:is_alphanumeric, " ")).to be false
     end
   end
 
   context "is_numeric" do
    it "returns true if the sequence is a numeric string , false otherwise." do
-   instance.send(:is_numeric, "123").should be true
-   instance.send(:is_numeric, "file").should be false
-   instance.send(:is_numeric, "file123").should be false
+   expect(instance.send(:is_numeric, "123")).to be true
+   expect(instance.send(:is_numeric, "file")).to be false
+   expect(instance.send(:is_numeric, "file123")).to be false
     end
   end
 end
 
 describe JSONFileReader do
   file_content = '[{ "name": "Test", "version": "1.0" }]'
-  let (:instance) {JSONFileReader.new(file_content, 'test', "test2"){|obj| obj.should_receive(:deserialize_json).and_return(true)}}
-  it { STDERR.should_receive(:puts).with('Failed to deserialize the following object:
+  let (:instance) {JSONFileReader.new(file_content, 'test', "test2"){|obj| expect(obj).to receive(:deserialize_json).and_return(true)}}
+  it { expect(STDERR).to receive(:puts).with('Failed to deserialize the following object:
 {"name"=>"Test", "version"=>"1.0"}')
     expect {instance.read_value}.to raise_error
      }
@@ -39,21 +39,21 @@ end
 describe "get_jsonreader_object" do
   it "returns a JSONFileReader object." do
     file_content = '[{ "name": "Test", "version": "1.0" }]'
-    get_jsonreader_object(file_content, "name").class.should eq JSONFileReader
+    expect(get_jsonreader_object(file_content, "name").class).to eq JSONFileReader
   end
 end
 
 describe "escape_unescaped_content" do
   it "escapes unescaped content." do
     file_content = '[{ "name": "Test", "version": "1.0" }]'
-    escape_unescaped_content(file_content).should eq "[{ \"name\": \"Test\", \"version\": \"1.0\" }]\n"
+    expect(escape_unescaped_content(file_content)).to eq "[{ \"name\": \"Test\", \"version\": \"1.0\" }]\n"
   end
 end
 
 describe "value_from_json_file" do
   it "returns json value from the supplied json file_content." do
     file_content = '[{ "name": "Test", "version": "1.0" }]'
-    value_from_json_file(file_content, "name").should eq "Test"
+    expect(value_from_json_file(file_content, "name")).to eq "Test"
   end
 
   it "returns json multi-lined value from the supplied json file_content." do
@@ -61,11 +61,11 @@ describe "value_from_json_file" do
     second line
     third line
     " }]'
-    value_from_json_file(file_content, "data").should eq "first line\nsecond line\nthird line\n"
+    expect(value_from_json_file(file_content, "data")).to eq "first line\nsecond line\nthird line\n"
   end
 
   it "returns json multi-lined value from the supplied json file." do
     file =  File.expand_path(File.dirname("spec/assets/*"))+"/test_json_file.json"
-    value_from_json_file(file, "data").should eq "first line\nsecond line\nthird line\n"
+    expect(value_from_json_file(file, "data")).to eq "first line\nsecond line\nthird line\n"
   end
 end
