289fbd2587c90acb53f43b1d09c8bbd1394c28a5
Vasu1105 <vasundhara.jagdale@clogeny.com>
2015-09-09 22:21:08 +0530

    Fixed uninstall for centos

--- ChefExtensionHandler/uninstall.sh
+++ ChefExtensionHandler/uninstall.sh
@@ -4,6 +4,6 @@ SCRIPT=$(readlink -f "$0")
 
 CHEF_EXT_DIR=$(dirname "$SCRIPT")
 
-echo $CHEF_EXT_DIR
+echo $CHEF_EXT_DIR >> /var/log/azure/custom.log
 
-sh $CHEF_EXT_DIR/bin/chef-uninstall.sh
+$CHEF_EXT_DIR/bin/chef-uninstall.sh >> /var/log/azure/custom.log
