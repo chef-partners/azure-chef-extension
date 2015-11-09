3ac761ebc107efdddbff9ca8b1e13d320308bc6b
aliasgar16 <aliasgar.batterywala@clogeny.com>
2015-11-09 14:05:23 +0530

    Removed changes done in Windows code for chef-client version as Windows WaAgent has issues where config file is unavailable before installation.

--- spec/ps_specs/chef-install.Tests.ps1
+++ spec/ps_specs/chef-install.Tests.ps1
@@ -19,59 +19,6 @@ Invoke-Expression $code
 $sharedHelper = $here.Replace("\spec\ps_specs", "\ChefExtensionHandler\bin\shared.ps1")
 . $sharedHelper
 
-Describe "#Get-Settings-File" {
-
-  In $TestDrive {
-    $configRootPath = ".\Packages\Plugin\ChefExtensionHandler\RuntimeSettings"
-    Setup -Dir $configRootPath
-    mock Chef-GetExtensionRoot { return $configRootPath }
-  
-    It "finds no config file and so exits with error message" {
-      $configFile = ""
-      Get-Settings-File | should Be("[$(Get-Date)] No config file found !!")
-    }
-  
-    It "returns runtime config/settings file path" {
-	  $configFile = "$configRootPath\0.settings"
-	  Setup -File $configFile
-      $configFilePath = Get-Settings-File
-      $configFilePath | should Match($configFile)
-      Remove-Item $configFile
-    }
-    Assert-MockCalled Chef-GetExtensionRoot -Times 1
-    Assert-VerifiableMocks
-	Remove-Item $configRootPath
-  }
-}
-
-Describe "#Get-Chef-Version" {
-
-  In $TestDrive {
-  
-    $settingsFile = ".\Packages\Plugin\ChefExtensionHandler\RuntimeSettings\0.settings"
-    Setup -File $settingsFile
-	mock Get-Settings-File { return $settingsFile }
-	
-    It "returns empty string" {
-      $config_data = '{"runtimeSettings":[{"handlerSettings":{"protectedSettingsCertThumbprint":"some_thumbprint","protectedSettings":"some_key","publicSettings":{"client_rb":"chef_server_url \t \"https://api.opscode.com/organizations/some_org\"\nvalidation_client_name\t\"some_org-validator\"","runlist":"","autoUpdateClient":"false","deleteChefConfig":"false","custom_json_attr":{},"bootstrap_options":{"chef_server_url":"https://api.opscode.com/organizations/some_org","validation_client_name":"some_org-validator"}}}}]}'
-      $config_data | Set-Content $settingsFile
-      $chefVersion = Get-Chef-Version
-      $chefVersion | should Match("")
-      Remove-Item $settingsFile
-    }
-  
-    It "returns chef-version to be installed" {
-      $config_data = '{"runtimeSettings":[{"handlerSettings":{"protectedSettingsCertThumbprint":"some_thumbprint","protectedSettings":"some_key","publicSettings":{"client_rb":"chef_server_url \t \"https://api.opscode.com/organizations/some_org\"\nvalidation_client_name\t\"some_org-validator\"","runlist":"","autoUpdateClient":"false","deleteChefConfig":"false","custom_json_attr":{},"bootstrap_options":{"chef_server_url":"https://api.opscode.com/organizations/some_org","validation_client_name":"some_org-validator","bootstrap_version":"12.3.2"}}}}]}'
-      $config_data | Set-Content $settingsFile
-      $chefVersion = Get-Chef-Version
-      $chefVersion | should Match("12.3.2")
-      Remove-Item $settingsFile
-    }
-	Assert-MockCalled Get-Settings-File -Times 1
-	Assert-VerifiableMocks
-  }
-}
-
 describe "#Install-ChefClient" {
   it "install chef and azure chef extension gem successfully" {
     # create temp powershell file for mock Get-SharedHelper
