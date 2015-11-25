c9d09309f894d0c5d084d0a16d3c3e289a3c5f82
NimishaS <nimisha.sharad@clogeny.com>
2015-11-25 18:27:35 +0530

    Creating .auto_update_false file in disable

--- ChefExtensionHandler/bin/chef-disable.rb
+++ ChefExtensionHandler/bin/chef-disable.rb
@@ -2,22 +2,45 @@
 # Ruby code that runs the original chef-client and updates the azure extension status
 
 require 'chef/azure/commands/disable'
+require 'chef/azure/commands/enable'
+include ChefAzure::Shared
+include ChefAzure::Config
+include ChefAzure::Reporting
 
 extension_root = File.expand_path(File.dirname(File.dirname(__FILE__)))
 puts "extension_root --> #{extension_root}"
 
 chef_disable_args = ARGV
 
-if(File.exist?('/etc/chef/.auto_update_false') || File.exist?('C:/chef/.auto_update_false'))
-  puts "Not doing disable since autoUpdateClient=false"
-  report_status_to_azure "chef-service enabled. Update failed as autoUpdateClient=false", "success"
-  report_heart_beat_to_azure(AzureHeartBeat::READY, 0, "chef-service is enabled. Update failed as autoUpdateClient=false")
-  exit 1
-else
-  puts "Creating DisableChef object with #{chef_disable_args}..."
-  command = DisableChef.new(extension_root, chef_disable_args)
+#Delete .auto_update_false file if it already exists
+File.delete("C:/chef/.auto_update_false") if File.exist?("C:/chef/.auto_update_false")
 
-  puts "Running Chef extension disable command..."
-  exit_code = command.run
-  exit exit_code
+#Get handler settings file
+@azure_heart_beat_file, @azure_status_folder, @azure_plugin_log_location, @azure_config_folder, @azure_status_file = read_config(extension_root)
+files = Dir.glob("#{File.expand_path(@azure_config_folder)}/*.settings").sort
+handler_settings_file = files.last if files and not files.empty?
+
+if windows?
+  total_extension_folders = Dir.entries("C:/Packages/Plugins/Chef.Bootstrap.WindowsAzure.ChefClient").select {|f| !File.directory? f}.count
 end
+
+#If total_extension_folders > 1 and disable is called from an old extension version, that means disable is called from Update
+if (total_extension_folders > 1 && extension_root != find_highest_extension_version(extension_root))
+  auto_update_client = value_from_json_file(handler_settings_file,'runtimeSettings','0','handlerSettings', 'publicSettings', 'autoUpdateClient')
+
+  if(auto_update_client != "true")
+    puts "Not doing extension disable as autoUpdateClient=false"
+    File.write("C:/chef/.auto_update_false", "autoUpdateClient=false")
+    report_heart_beat_to_azure(AzureHeartBeat::READY, 0, "chef-service is enabled. Update failed as autoUpdateClient=false")
+    report_status_to_azure "chef-service enabled. Update failed as autoUpdateClient=false", "success"
+    exit
+  end
+end
+
+puts "Creating DisableChef object with #{chef_disable_args}..."
+command = DisableChef.new(extension_root, chef_disable_args)
+
+puts "Running Chef extension disable command..."
+exit_code = command.run
+exit exit_code
+
--- ChefExtensionHandler/bin/chef-enable.rb
+++ ChefExtensionHandler/bin/chef-enable.rb
@@ -8,16 +8,9 @@ puts "#{Time.now} extension_root --> #{extension_root}"
 
 chef_enable_args = ARGV
 
-if(File.exist?('/etc/chef/.auto_update_false') || File.exist?('C:/chef/.auto_update_false'))
-  puts "Not doing enable since autoUpdateClient=false"
-  report_status_to_azure "chef-service enabled. Update failed as autoUpdateClient=false", "success"
-  report_heart_beat_to_azure(AzureHeartBeat::READY, 0, "chef-service is enabled. Update failed as autoUpdateClient=false")
-  exit 1
-else
-  puts "#{Time.now} Creating EnableChef object with #{chef_enable_args}..."
-  command = EnableChef.new(extension_root, chef_enable_args)
+puts "#{Time.now} Creating EnableChef object with #{chef_enable_args}..."
+command = EnableChef.new(extension_root, chef_enable_args)
 
-  puts "#{Time.now} Running Chef extension enable command..."
-  exit_code = command.run
-  exit exit_code
-end
+puts "#{Time.now} Running Chef extension enable command..."
+exit_code = command.run
+exit exit_code
