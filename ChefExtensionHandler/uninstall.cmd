1251da20a95308f0ee3d4cd12df3b85a66c462dc
aliasgar16 <aliasgar.batterywala@clogeny.com>
2015-12-15 21:23:22 +0530

    Added uninstall_chef_client_flag code for Windows platform in uninstall.cmd script.

--- ChefExtensionHandler/uninstall.cmd
+++ ChefExtensionHandler/uninstall.cmd
@@ -1,3 +1,4 @@
+@echo off
 
 set CHEF_EXT_DIR=%~dp0
 
@@ -6,7 +7,7 @@ echo %CHEF_EXT_DIR%
 call:Get-Settings-File
 call:Get-Uninstall-Chef-Client-Flag
 
-if [%uninstall_chef_client%] == [true] (
+if [%uninstall_chef_client_flag%] == [true] (
   powershell -nologo -noprofile -executionpolicy unrestricted Import-Module %CHEF_EXT_DIR%bin\chef-uninstall.psm1;Uninstall-ChefClient
 )
 GOTO:EOF
@@ -21,9 +22,18 @@ GOTO:EOF
 
 :Get-Uninstall-Chef-Client-Flag
   if [%config_file_path%] == [] (
-    set uninstall_chef_client=false
-    echo %uninstall_chef_client%
+    set uninstall_chef_client_flag=false
   ) else (
-      ::TODO retrieve uninstall chef client flag from settings file
+      set ps_version_cmd=powershell -nologo -noprofile -executionpolicy unrestricted -Command "$PSVersionTable.PSVersion.Major"
+      for /f "delims=" %%I in ('%ps_version_cmd%') do set "ps_version=%%I"
+      if [%ps_version%] leq [2] (
+        set uninstall_chef_client_ps2_cmd=powershell -nologo -noprofile -executionpolicy unrestricted -Command "[System.Reflection.Assembly]::LoadWithPartialName(\"System.Web.Extensions\") > $null;$config_json_contents = Get-Content %config_file_path% -Raw;$serObj = New-Object System.Web.Script.Serialization.JavaScriptSerializer;$psObj = New-Object PSObject -Property $serObj.DeserializeObject($config_json_contents);$uninstall_chef_client_ps2 = $psObj.runtimeSettings.handlerSettings.publicSettings.uninstallChefClient;$uninstall_chef_client_ps2"
+
+        for /f "delims=" %%J in ('%uninstall_chef_client_ps2_cmd%') do set "uninstall_chef_client_flag=%%J"
+      ) else if [%ps_version%] gtr [2] (
+          set uninstall_chef_client_ps3_cmd=powershell -nologo -noprofile -executionpolicy unrestricted -Command "$settingsData = Get-Content %config_file_path% -Raw | ConvertFrom-Json;$uninstall_chef_client_ps3 = $settingsData.runtimesettings.handlersettings.publicsettings.uninstallChefClient;$uninstall_chef_client_ps3"
+
+          for /f "delims=" %%K in ('%uninstall_chef_client_ps3_cmd%') do set "uninstall_chef_client_flag=%%K"
+        )
     )
 GOTO:EOF
