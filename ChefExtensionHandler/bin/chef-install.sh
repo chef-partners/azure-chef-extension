dd811de56e74e62cf5125b01f36479d2dc8082cc
dheerajd-msys <dheeraj.dubey@msystechnologies.com>
2016-05-13 12:25:49 +0530

    Updated logs and removed curl related code

--- ChefExtensionHandler/bin/chef-install.sh
+++ ChefExtensionHandler/bin/chef-install.sh
@@ -48,7 +48,7 @@ get_chef_version() {
 
 wget_check(){
   echo "Checking for wget..."
-  if command -v curl > /dev/null; then
+  if command -v wget > /dev/null; then
     echo "Detected wget..."
   else
     echo "Installing wget..."
@@ -60,34 +60,14 @@ wget_check(){
   fi
 }
 
-curl_check(){
-  echo "Checking for curl..."
-  if command -v curl > /dev/null; then
-    echo "Detected curl..."
-  else
-    echo "Installing curl..."
-    if [ "$1" = "centos" -o "$1" = "rhel" ]; then
-      yum install -d0 -e0 -y curl
-    else
-      apt-get install -q -y curl
-    fi
-  fi
-}
-
 chef_install_from_script(){
-    chef_version=$(get_chef_version &)
     echo "Reading chef-client version from settings file"
+    chef_version=$(get_chef_version &)
     echo "Call for Checking linux distributor"
     platform=$(get_linux_distributor)
     wget_check $platform
     wget -O /tmp/$platform-install.sh https://omnitruck.chef.io/install.sh
-      if ! test -s /tmp/$platform-install.sh; then       
-        echo "Using curl to download install.sh"
-        curl_check $platform
-        curl -L -o /tmp/$platform-install.sh https://omnitruck.chef.io/install.sh
-      fi
-      echo "Install.sh script downloaded successfully"
-    echo "chef client version $chef_version"
+    echo "Install.sh script downloaded at /tmp/$platform-install.sh"
     if [ "$chef_version" = "No config file found !!" ]; then
       echo "Configuration error. Azure chef extension Settings file missing."
       exit 1
@@ -98,9 +78,8 @@ chef_install_from_script(){
       echo "Installing chef client with version $chef_version"
       sh /tmp/$platform-install.sh -v $chef_version
     fi
+    echo "Deleting Install.sh script present at /tmp/$platform-install.sh"
     rm /tmp/$platform-install.sh -f
-    echo "chef client installed sucessfully"
-    echo `knife -v`
 }
 
 ########### Script starts from here ##################
