98da5980bd8f13c601e9d0c926bf0c73f036ed3f
dheerajd-msys <dheeraj.dubey@msystechnologies.com>
2016-06-27 12:07:54 +0530

    Code changes for removing autoUpdateClient,deleteChefCofig and deleteChefCofig flags on Linux Platforms

--- ChefExtensionHandler/bin/chef-uninstall.sh
+++ ChefExtensionHandler/bin/chef-uninstall.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 # returns script dir
 get_script_dir(){
   SCRIPT=$(readlink -f "$0")
@@ -14,80 +15,16 @@ chef_extension_root=$commands_script_path/../
 
 read_environment_variables $chef_extension_root
 
-#funtions to delete ubuntu chef configuration files i.e. /etc/chef
-remove_chef_config(){
-  if [ ! -z $delete_chef_config ] && [ $delete_chef_config = "true" ] ; then
-    echo "Deleteing chef configurations directory /etc/chef."
-    rm -rf /etc/chef || true
-  fi
-}
-
-# Function to uninstall ubuntu chef_pkg
-uninstall_ubuntu_or_debian_chef_package(){
-  pkg_name=`dpkg -l | grep chef | awk '{print $2}'`
-  echo "[$(date)] Uninstalling package $pkg_name ..."
-  apt-get remove --purge $pkg_name -y
-  check_uninstallation_status
-}
-
-# Function to Uninstall centOS or RHEL chef_pkg
-uninstall_centos_or_rhel_chef_package(){
-  pkg_name=`rpm -qi chef | grep Name | awk '{print $3}'`
-  if test "$pkg_name" = "chef" ; then
-    echo "[$(date)] Uninstalling package $pkg_name ..."
-    yum remove $pkg_name -y
-    check_uninstallation_status
-  else
-    echo "[$(date)] No Package found to uninstall!!!"
-  fi
-}
-
-check_uninstallation_status(){
-  if [ $? -eq 0 ]; then
-    echo "[$(date)] Package $pkg_name uninstalled successfully."
-  else
-    echo "[$(date)] Unable to uninstall package Chef."
-  fi
-}
-
 ########### Script starts from here ###################
-linux_distributor=$(get_linux_distributor)
-
-auto_update_false=/etc/chef/.auto_update_false
 
 export PATH=/opt/chef/embedded/bin:/opt/chef/bin:$PATH
 
-if [ -f $auto_update_false ]; then
-  uninstall_chef_client=`ruby -e "require 'chef/azure/helpers/parse_json';value_from_json_file_for_ps '$handler_settings_file','runtimeSettings','0','handlerSettings','publicSettings','uninstallChefClient'"`
-  if [ "$uninstall_chef_client" = "true" ]; then
-    echo "Invalid config specified...uninstallChefClient flag cannot be true when autoUpdateClient flag is false." >> /var/log/azure/custom.log
-  fi
-  exit 1
-fi
-
 update_process_descriptor=/etc/chef/.updating_chef_extension
-
-called_from_update=$1
 
 if [ -f $update_process_descriptor ]; then
   echo "[$(date)] Not doing uninstall, as the update process is running"
   rm $update_process_descriptor
 else
-
-  chef_ext_dir=`dirname $commands_script_path`
-
-  if [ "$called_from_update" = "update" ]; then
-    waagentdir="$(dirname "$chef_ext_dir")"
-    previous_extension=`ls "$waagentdir" | grep Chef.Bootstrap.WindowsAzure.LinuxChefClient- | tail -2 | head -1`
-    previous_extension="$waagentdir/$previous_extension"
-    handler_settings_file=`ls $previous_extension/config/*.settings -S -r | head -1`
-  else
-    handler_settings_file=`ls $chef_ext_dir/config/*.settings -S -r | head -1`
-  fi
-
-  # Reading deleteChefConfig value from settings file
-  delete_chef_config=`ruby -e "require 'chef/azure/helpers/parse_json';value_from_json_file_for_ps '$handler_settings_file','runtimeSettings','0','handlerSettings','publicSettings','deleteChefConfig'"`
-
   # Uninstall the custom gem
   azure_chef_extn_gem=`gem list azure-chef-extension | grep azure-chef-extension | awk '{print $1}'`
 
@@ -102,39 +39,4 @@ else
   else
     echo "[$(date)] Gem azure-chef-extension is not installed !!!"
   fi
-
-  case $linux_distributor in
-    "ubuntu"|"debian")
-      echo "Linux Distributor: ${linux_distributor}"
-      remove_chef_config
-      uninstall_ubuntu_or_debian_chef_package
-      ;;
-    "centos"|"rhel")
-      echo "Linux Distributor: ${linux_distributor}"
-      remove_chef_config
-      uninstall_centos_or_rhel_chef_package
-      ;;
-    *)
-      echo "Unknown Distributor: $linux_distributor"
-      exit 1
-      ;;
-  esac
-
-  # remove /opt/chef if it doesn't get deleted by the uninstallation process
-  if [ -d /opt/chef ]; then
-    rm -rf /opt/chef
-    echo "Forcibly deleted /opt/chef directory."
-  fi
-
-  PREFIX="/usr"
-  # ensure symlinks are gone, so that failures to recreate them get caught
-  rm -f $PREFIX/bin/chef-client || true
-  rm -f $PREFIX/bin/chef-solo || true
-  rm -f $PREFIX/bin/chef-apply || true
-  rm -f $PREFIX/bin/chef-shell || true
-  rm -f $PREFIX/bin/knife || true
-  rm -f $PREFIX/bin/shef || true
-  rm -f $PREFIX/bin/ohai || true
-
-  echo "Deleted related symlinks."
 fi
--- ChefExtensionHandler/uninstall.sh
+++ ChefExtensionHandler/uninstall.sh
@@ -1,31 +1,10 @@
 #!/bin/sh
 
-get_config_settings_file() {
-  config_files_path="$1/config/*.settings"
-  config_file_name=`ls $config_files_path 2>/dev/null | sort -V | tail -1`
-
-  echo $config_file_name
-}
-
-get_uninstall_chef_client_flag() {
-  if [ -z "$1" ]; then
-    echo "false"
-  else
-    export PATH=/opt/chef/bin/:/opt/chef/embedded/bin:$PATH
-    uninstall_chef_client_flag=`ruby -e "require 'chef/azure/helpers/parse_json';value_from_json_file_for_ps '$1','runtimeSettings','0','handlerSettings','publicSettings','uninstallChefClient'"`
-    echo $uninstall_chef_client_flag
-  fi
-}
-
 SCRIPT=$(readlink -f "$0")
 
 CHEF_EXT_DIR=$(dirname "$SCRIPT")
 
 echo $CHEF_EXT_DIR >> /var/log/azure/custom.log
 
-config_file_name=$(get_config_settings_file $CHEF_EXT_DIR)
-uninstall_chef_client=$(get_uninstall_chef_client_flag $config_file_name)
+$CHEF_EXT_DIR/bin/chef-uninstall.sh >> /var/log/azure/custom.log
 
-if [ "$uninstall_chef_client" = "true" ]; then
-  $CHEF_EXT_DIR/bin/chef-uninstall.sh >> /var/log/azure/custom.log
-fi
