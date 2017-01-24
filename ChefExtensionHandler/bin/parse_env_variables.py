b2ae113cbe59a710c98d0b570e40dd72d00f7f8a
MSys <harikesh.kolekar@msystechnologies.com>
2017-01-24 16:15:03 +0530

    add strict false while parsing json in python code

--- ChefExtensionHandler/bin/parse_env_variables.py
+++ ChefExtensionHandler/bin/parse_env_variables.py
@@ -1,7 +1,7 @@
 import json
 import sys
 with open(sys.argv[1]) as data_file:
-    data = json.load(data_file)
+    data = json.load(data_file, strict=False)
 data = data['runtimeSettings'][0]['handlerSettings']['publicSettings']['environment_variables']
 comands=""
 for key, value in data.items():
