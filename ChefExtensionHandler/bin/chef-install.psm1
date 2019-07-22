c3a587fbca1e896937edeb27fc556ba82bdc5993
NAshwini <ashwini.nehate@msystechnologies.com>
2019-07-22 15:44:29 +0530

    Add msi_path option to install locally downloaded chef-client for windows
    Signed-off-by: NAshwini <ashwini.nehate@msystechnologies.com>

--- ChefExtensionHandler/bin/chef-install.psm1
+++ ChefExtensionHandler/bin/chef-install.psm1
@@ -55,25 +55,29 @@ function Install-ChefClient {
     Try {
       ## Get chef_pkg by matching "chef client" string with $_.Name
       $chef_pkg = Get-ChefPackage
-      if (-Not $chef_pkg) {
+      ## Get locally downloaded msi path string from config file.
+      $msi_path = Get-PublicSettings-From-Config-Json "msi_path" $powershellVersion
+      $daemon = Get-PublicSettings-From-Config-Json "daemon"  $powershellVersion
+      if ( $daemon -eq "none" ) {
+        $daemon = "auto"
+      }
+      if (-Not $daemon) {
+        $daemon = "service"
+      }
+      if (-Not $chef_pkg -and -Not $msi_path ) {
         $chef_package_version = Get-PublicSettings-From-Config-Json "bootstrap_version" $powershellVersion
         $chef_package_channel = Get-PublicSettings-From-Config-Json "bootstrap_channel" $powershellVersion
-        $daemon = Get-PublicSettings-From-Config-Json "daemon"  $powershellVersion
 
-        if ( $daemon -eq "none" ) {
-          $daemon = "auto"
-        }
         if (-Not $chef_package_version) {
           $chef_package_version = "14" # Until Chef-15 is Verified
         }
         if (-Not $chef_package_channel) {
           $chef_package_channel = "stable"
         }
-        if (-Not $daemon) {
-          $daemon = "service"
-        }
 
         iex (new-object net.webclient).downloadstring('https://omnitruck.chef.io/install.ps1');install -daemon $daemon -version $chef_package_version -channel $chef_package_channel
+      } ElseIf ( -Not $chef_pkg -and $msi_path ) {
+        Install-ChefMsi $msi_path $daemon
       }
       $completed = $true
     }
@@ -95,6 +99,27 @@ function Install-ChefClient {
   $env:Path = "C:\\opscode\\chef\\bin;C:\\opscode\\chef\\embedded\\bin;" + $env:Path
   $chefExtensionRoot = Chef-GetExtensionRoot
   Install-AzureChefExtensionGem $chefExtensionRoot
+}
+
+Function Install-ChefMsi($msi, $addlocal) {
+  if ($addlocal -eq "service") {
+    $p = Start-Process -FilePath "msiexec.exe" -ArgumentList "/qn /i $msi ADDLOCAL=`"ChefClientFeature,ChefServiceFeature`"" -Passthru -Wait -NoNewWindow
+  }
+  ElseIf ($addlocal -eq "task") {
+    $p = Start-Process -FilePath "msiexec.exe" -ArgumentList "/qn /i $msi ADDLOCAL=`"ChefClientFeature,ChefSchTaskFeature`"" -Passthru -Wait -NoNewWindow
+  }
+  ElseIf ($addlocal -eq "auto") {
+    $p = Start-Process -FilePath "msiexec.exe" -ArgumentList "/qn /i $msi" -Passthru -Wait -NoNewWindow
+  }
+
+  $p.WaitForExit()
+  if ($p.ExitCode -eq 1618) {
+    Write-Host "$((Get-Date).ToString()) - Another msi install is in progress (exit code 1618), retrying ($($installAttempts))..."
+    return $false
+  } elseif ($p.ExitCode -ne 0) {
+    throw "msiexec was not successful. Received exit code $($p.ExitCode)"
+  }
+  return $true
 }
 
 function Get-SharedHelper {
